8921ff8b0e1cb43e67fcafa993402da4471b785c
BaranovAnton <baranov_anton@inbox.ru>
2020-03-23 10:33:18 +0300

    add: Singleton pattern

---
+++ Assets/Singleton/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+    private static Singleton instance;
+
+    private Singleton() { }
+
+    public static Singleton Instance()
+    {
+        if (instance == null)
+            instance = new Singleton();
+
+        return instance;
+    }
+}
