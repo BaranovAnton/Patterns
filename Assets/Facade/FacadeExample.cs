024e431bf05e734d4e99834a2a82094371bf3a66
BaranovAnton <baranov_anton@inbox.ru>
2020-03-30 14:08:07 +0300

    add: Facade pattern

---
+++ Assets/Facade/FacadeExample.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacadeExample : MonoBehaviour
+{
+    JobPathFacade jobPathFacade = new JobPathFacade("Flat", "My garage", "Private parking", "Traffic", "Ehyyy highway", "Hi Joe", "Otis elevator", "Open space office");
+
+    void Start()
+    {
+        jobPathFacade.GoToTheOffice();
+        jobPathFacade.GoHome();
+    }
+}
---
+++ Assets/Facade/JobPathFacade.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JobPathFacade
+{
+    string home;
+    string garage;
+    string parking;
+    string road;
+    string highway;
+    string office_parking;
+    string elevator;
+    string office;
+
+    public JobPathFacade(string home, string garage, string parking, string road, string highway, string office_parking, string elevator, string office)
+    {
+        this.home = home;
+        this.garage = garage;
+        this.parking = parking;
+        this.road = road;
+        this.highway = highway;
+        this.office_parking = office_parking;
+        this.elevator = elevator;
+        this.office = office;
+    }
+
+    public void GoToTheOffice()
+    {
+        Debug.Log("Go to the office...");
+        Debug.Log(home);
+        Debug.Log(garage);
+        Debug.Log(parking);
+        Debug.Log(road);
+        Debug.Log(highway);
+        Debug.Log(office_parking);
+        Debug.Log(elevator);
+        Debug.Log(office);
+    }
+
+    public void GoHome()
+    {
+        Debug.Log("Go home...");
+    }
+}
